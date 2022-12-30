52af1ac20c1ec63f43cefde5e3b859e95b919ebc
RanGedamo <mcrangedamo@gmail.com>
2022-12-31 00:57:08 +0200

    server ran gedamo

---
+++ server/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+// require('./DB')
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`listen to port : ${process.env.PORT}`);
+})
+
+
+
+
+
+/* ----------------------------------------------------- */
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+};
+
+/* ----------------------------------------------------- */
+
+
