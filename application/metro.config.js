183cf951409a182c8bf32497ffd16cdac5f57dd6
RanGedamo <mcrangedamo@gmail.com>
2023-01-01 15:31:11 +0200

    fix metro.config apllication ran gedamo

--- application/metro.config.js
+++ application/metro.config.js
@@ -5,17 +5,6 @@
  * @format
  */
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f9a7299b (src)
-=======
->>>>>>> f9a7299b (src)
-=======
->>>>>>> 217666ab (server)
 // module.exports = {
 //   transformer: {
 //     getTransformOptions: async () => ({
@@ -36,40 +25,11 @@ module.exports = (async () => {
   return {
     transformer: {
           getTransformOptions: async () => ({
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 217666ab (server)
-=======
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
->>>>>>> 06b8cbbe (app)
-<<<<<<< HEAD
-=======
->>>>>>> f9a7299b (src)
-=======
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
->>>>>>> 06b8cbbe (app)
-=======
->>>>>>> f9a7299b (src)
-=======
->>>>>>> 217666ab (server)
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
       },
     }),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 217666ab (server)
     babelTransformerPath: require.resolve("react-native-svg-transformer")
   },
     resolver: {
@@ -78,31 +38,3 @@ module.exports = {
     }
   };
 })();
-=======
-  },
-};
->>>>>>> 06b8cbbe (app)
-<<<<<<< HEAD
-=======
-    babelTransformerPath: require.resolve("react-native-svg-transformer")
-  },
-=======
-    babelTransformerPath: require.resolve("react-native-svg-transformer")
-  },
->>>>>>> f9a7299b (src)
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
-  };
-})();
-<<<<<<< HEAD
->>>>>>> f9a7299b (src)
-=======
-  },
-};
->>>>>>> 06b8cbbe (app)
-=======
->>>>>>> f9a7299b (src)
-=======
->>>>>>> 217666ab (server)
