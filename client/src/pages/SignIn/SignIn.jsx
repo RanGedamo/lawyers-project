75a9bbfe944ffe060044469b6b9f943c6f2411c7
RanGedamo <mcrangedamo@gmail.com>
2023-01-04 00:42:29 +0200

    facebook

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { GoogleButton } from 'react-google-button';
 import { UserAuth } from '../../context/UserContext';
+import {FacebookLoginButton} from 'react-social-login-buttons';
+
 // import { useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
-  const { googleSignIn, user } = UserAuth();
+  const { googleSignIn,facebookSignIn, user } = UserAuth();
   // const navigate = useNavigate();
 
   const handleGoogleSignIn = async () => {
@@ -23,11 +25,26 @@ const SignIn = () => {
   //   }
   // }, [user]);
 
+
+
+  const signInWithFacebook = async () => {
+
+      try {
+        await facebookSignIn().then(res=>console.log(res));
+        
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
   return (
     <div className='  signIn'>
       <h1 className='text-center text-3xl font-bold py-8'>Sign in</h1>
       <div className='max-w-[240px] m-auto py-4 d-flex justify-content-center'>
         <GoogleButton onClick={handleGoogleSignIn} />
+      </div>
+      <div className='max-w-[240px] m-auto py-4 d-flex justify-content-center'>
+        <FacebookLoginButton style={{width:"250px"}} onClick={signInWithFacebook} />
       </div>
     </div>
   );
