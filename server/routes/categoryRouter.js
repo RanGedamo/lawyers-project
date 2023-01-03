56d470ec261981735764bd7ea91105c803a4e5b5
RanGedamo <mcrangedamo@gmail.com>
2023-01-03 15:58:20 +0200

    firebase with google - ran gedamo

--- server/routes/categoryRouter.js
+++
@@ -1,7 +0,0 @@
-const router=require("express").Router();
-const {getCategory,createCategory}=require("../controller/categoryCtrl")
-
-router.get("/",getCategory)
-router.post("/",createCategory)
-
-module.exports=router
--- server/routes/lawyeRouter.js
+++
@@ -1,7 +0,0 @@
-const router=require("express").Router();
-const {getLawyers,createLayer}=require("../controller/lawyerCtrl")
-
-router.get("/",getLawyers)
-router.post("/",createLayer)
-
-module.exports=router
