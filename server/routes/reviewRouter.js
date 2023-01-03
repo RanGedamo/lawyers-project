57467a979cf6e979f8c0aece673e01c36e1ce781
RanGedamo <mcrangedamo@gmail.com>
2023-01-03 15:58:20 +0200

    firebase with google - ran gedamo

--- server/routes/reviewRouter.js
+++
@@ -1,7 +0,0 @@
-const router=require("express").Router();
-const {getReview,createReview}=require("../controller/reviewCtrl")
-
-router.get("/",getReview)
-router.post("/",createReview)
-
-module.exports=router
