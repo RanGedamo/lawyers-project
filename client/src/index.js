da204f3da270a90997c42ab316577e52b10ff022
RanGedamo <mcrangedamo@gmail.com>
2023-01-05 01:16:04 +0200

    chat ran

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AuthContextProvider } from './context/UserContext';
-import { ChatProvider } from './context/ChatProvider';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <AuthContextProvider>
-    <ChatProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ChatProvider>
-  </AuthContextProvider>
 
-);
+
+ReactDOM.render(  
+<AuthContextProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+</AuthContextProvider>, document.getElementById('root'));
