0c91c74ed2b6723a17c37e31773a79e6eaeeeeb9
RanGedamo <mcrangedamo@gmail.com>
2023-01-05 01:16:04 +0200

    chat ran

--- client/src/Routing.jsx
+++ client/src/Routing.jsx
@@ -1,17 +1,21 @@
 import { Route, Routes } from "react-router-dom"
+import Chat from "./component/chat/Chat"
 import Home from "./pages/Home/Home"
 import SignUpSignInForm from "./pages/signUpSignInForm"
 
 
 
-const Routing = ()=>{
-    
-    return(
+const Routing = () => {
+
+    return (
         <div className="routing main">
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="SignIn" element={<signUpSignInForm/>}/>
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="SignIn" element={<signUpSignInForm />} />
+            </Routes>
+            <div className="chat">
+                <Chat />
+            </div>
         </div>
     )
 }
