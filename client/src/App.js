cf6dad0c5e997415f28c1eb51cf546fafa202062
Avielteka <aviek2260@gmail.com>
2023-01-09 22:25:55 +0200

    ChartjsAviel

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import Footer from './component/Footer/Footer'
 import Header from './component/Header/Header';
 import PopUpRole from './component/PopUpRole/PopUpBtn';
 import Routing from './Routing';
-import Navbar from "./component/Navbar/Navbar"
+import Navbar from "./component/Navbar/Navbar" 
+import Chart from "./component/chart/Chart"
 export default function App() {
   
   return (
     <div className="App">
-      <BrowserRouter>
-      {/* <Header /> */}
-      <Navbar/>
-      <Routing />
+      {/* <BrowserRouter> */}
+       <Header /> 
+       {/* <Navbar/> */}
+      {/*<Routing />
       <Footer />
-      </BrowserRouter>
+      </BrowserRouter> */}
+       <Chart/> 
     </div>
   );
 };
---
+++ client/src/component/chart/Chart.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+export default function AreaChart({item}) {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'New Registers Users',
+      },
+    },
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  const data = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: 'Added Users',
+        data: [233,343,432,32434],
+        borderColor: 'rgb(53, 162, 235)',
+        tension:0.4,
+        backgroundColor: 'rgba(21, 50, 235, 0.6)',
+      },
+    ],
+  };
+  return <Line  width={500} options={options} data={data} />;
+}
