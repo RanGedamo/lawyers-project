d9f6111b5240d98126048e4466f20221f4e59204
DanielMalede <danielmalede@gmail.com>
2023-01-09 21:45:43 +0200

    fixing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './component/Footer/Footer'
-import Header from './component/Header/Header';
 import PopUpRole from './component/PopUpRole/PopUpBtn';
 import Routing from './Routing';
 import Navbar from "./component/Navbar/Navbar"
