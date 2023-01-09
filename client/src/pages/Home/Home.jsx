aa0d285a7550080cd158a83dddd2d70f81401730
NetanelTesfay <netaneltes66@gmail.com>
2023-01-10 01:21:00 +0200

    add an array to sidder.js ,open Filter Component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,32 +5,34 @@ import SupportBoard from '../../component/Section/SupportBoard';
 // import Category from '../../component/cards/Category';
 // import ContactUs from '../../components/Section/ContactUs';
 import { SimpleGrid, Box, VStack } from '@chakra-ui/react';
+import Filter from '../../component/filter/Filter';
 
 export default function Home() {
   return (
-    <VStack spacing={4} align="center">
-      <Box>
-        <Hero />
-      </Box>
-      <Box bg={'blue.700'} borderRadius="70px">
-        <SupportBoard />
-      </Box>
-      <Box>
-        {/* <Category /> */}
-      </Box>
-      <Box>
-        <SimpleGrid columns={2} spacingX="40px" spacingY="20px">
-          <Box height="80px">
-            {/* <ContactUs bg={'blue.400'} /> */}
-          </Box>
-          <Box height="80px">
-            <Scheduling bg={'blue.400'} />
-          </Box>
-        </SimpleGrid>
-      </Box>
-      <Box>
-        <HowToHire />
-      </Box>
-    </VStack>
+    // <VStack spacing={4} align="center">
+    //   <Box>
+    //     <Hero />
+    //   </Box>
+    //   <Box bg={'blue.700'} borderRadius="70px">
+    //     <SupportBoard />
+    //   </Box>
+    //   <Box>
+    //     {/* <Category /> */}
+    //   </Box>
+    //   <Box>
+    //     <SimpleGrid columns={2} spacingX="40px" spacingY="20px">
+    //       <Box height="80px">
+    //         {/* <ContactUs bg={'blue.400'} /> */}
+    //       </Box>
+    //       <Box height="80px">
+    //         <Scheduling bg={'blue.400'} />
+    //       </Box>
+    //     </SimpleGrid>
+    //   </Box>
+    //   <Box>
+    //     <HowToHire />
+    //   </Box>
+    // </VStack>
+    <Filter/>
   );
 }
