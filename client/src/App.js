bf1f7268ffbdb56589d777f4361540cfecbbd31e
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2023-01-10 23:54:10 +0200

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,19 @@ import LawyerPayment from "./component/cards/LawyerPayment";
 import PayPal from "./component/payPal/PayPal";
 import { Payment } from "./AppRoute/pagesRoute";
 import PopUpRole from "./component/pop/PopUpBtn";
+import { useSelector } from "react-redux";
+import { FireBaseConfig } from "./FireBaseConfig/FireBaseConfig";
+import { useEffect } from "react";
 
 export default function App() {
+
+ 
+ FireBaseConfig()
+
+  const user=useSelector((state)=>state.userData)
+  const facebook=useSelector((state)=>state.facebookSignIn)
+
+  console.log(user)
   return (
     <div className="App">
       <BrowserRouter>
