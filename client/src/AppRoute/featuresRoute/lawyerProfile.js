a93319783872cbe11d2469c76a795a25b30ed98c
RanGedamo <mcrangedamo@gmail.com>
2023-01-11 23:26:21 +0200

    fix import  Reviews jsx

--- client/src/AppRoute/featuresRoute/lawyerProfile.js
+++ client/src/AppRoute/featuresRoute/lawyerProfile.js
@@ -1,7 +1,7 @@
 import LawyerProfileCard from "../../component/cards/LawyerProfileCard";
 import Reviews from "../../component/cards/Reviews";
 import AreaChart from '../../component/chart/Chart';
-import ReviewsInput from '../../component/Reviews/Reviews';
+import ReviewsInput from '../../component/form/Reviews';
 import CommentSection from '../../component/commentSection/CommentSection';
 import LawyerCategories from '../../component/cards/LawyerCategories';
 
