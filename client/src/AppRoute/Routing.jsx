5c15fdce07cae56780947f9a1b427045d263b639
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2023-01-11 22:48:36 +0200

    payment

--- client/src/AppRoute/Routing.jsx
+++ client/src/AppRoute/Routing.jsx
@@ -8,7 +8,7 @@ export default function Routing() {
                 <Route path="/" element={<Home /> } />
                 <Route path={'/category/:id'} element={<Filter /> } />
                 <Route path="/lawyer/profile/:id" element={<LawyerProfile /> } />
-                <Route path="payment" element={<Payment/>} />
+                <Route path="/payment" element={<Payment/>} />
                 <Route path="*" element={<ErrorPage /> } />
             </Routes>
         </div>
--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import { MDBRow, MDBCol, MDBContainer } from "mdb-react-ui-kit";
+import AgreementCard from "../../component/cards/AgreementCard";
+import LawyerProfile from "../../component/cards/LawyerProfileCard";
+import LawyerPayment from "../../component/cards/LawyerPayment";
+import PaymentCard from "../../component/cards/PaymentCard";
+import "./Payment.css"
 function Payment() {
   return (
-    <div>Payment</div>
-  )
+    <div className="container-fluid h-100 paymentContainer">
+      <MDBRow className="">
+        <MDBCol md="4">
+          <MDBRow className="flex-column">
+            <MDBCol className="profilePayment">
+              <LawyerPayment />
+            </MDBCol>
+            <MDBCol className="paymentCard" >
+              <PaymentCard />
+            </MDBCol>
+          </MDBRow>
+        </MDBCol>
+        <MDBCol md="8" >
+          <MDBRow>
+            <MDBCol md="12"></MDBCol>
+            <MDBCol
+              md="12"
+              className="d-flex row justify-content-center align-align-items-center agreement "
+            >
+              <AgreementCard />
+            </MDBCol>
+            <MDBCol></MDBCol>
+          </MDBRow>
+        </MDBCol>
+      </MDBRow>
+    </div>
+  );
 }
 
-export default Payment
+export default Payment;
