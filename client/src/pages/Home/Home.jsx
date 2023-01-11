111aacbf13e65622c7be72fa9db7de0afbaae5c5
NetanelTesfay <netaneltes66@gmail.com>
2023-01-11 18:46:03 +0200

    home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,38 +3,41 @@ import Scheduling from "../../component/Section/Scheduling";
 import HowToHire from "../../component/Section/HowToHire";
 import SupportBoard from "../../component/Section/SupportBoard";
 // import Category from '../../component/cards/Category';
-import { GridItem, Box, Grid, Container } from "@chakra-ui/react";
 import ContactUs from "../../component/Section/ContactUs";
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+
 
 export default function Home() {
   return (
-    <>
-      <Container>
-        <Hero />
-      </Container>
-      <Grid
-        templateRows="repeat(5)"
-        templateColumns="repeat(6)"
-        gap={4}
-      >
-        <GridItem colSpan={4}>
-          <Box bg={"blue.700"} borderRadius="70px">
-            <SupportBoard />
-          </Box>
-        </GridItem>
-        {/* <GridItem colSpan={5} bg="papayawhip"> */}
-        {/* <Category /> */}
-        {/* </GridItem> */}
-        <GridItem colSpan={2}>
-          <ContactUs bg={"blue.400"} />
-        </GridItem>
-        <GridItem colSpan={2}>
-          <Scheduling bg={"blue.400"} />
-        </GridItem>
-        <GridItem colSpan={4}>
-          <HowToHire />
-        </GridItem>
-      </Grid>
-    </>
+    <MDBContainer>
+      <MDBContainer>
+      <Hero/>
+      </MDBContainer>
+    <MDBRow className='bg-light mb-3 rounded-9'>
+    <SupportBoard/>
+    </MDBRow>
+    <MDBRow className='bg-light mb-3'>
+      <MDBCol md='4'>
+        .col-md-4
+      </MDBCol>
+      <MDBCol md='4' offsetMd='4'>
+        .col-md-4 .offset-md-4
+      </MDBCol>
+    </MDBRow>
+    <MDBRow className='mb-3d d-flex justify-content-between'>
+        <MDBCol size='6' md='4'>
+          <ContactUs/>
+        </MDBCol>
+        <MDBCol size='6' md='4'>
+          <Scheduling/>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow className='bg-light mb-3'>
+      <MDBCol md='4' offsetMd='4'>
+      <HowToHire />
+      </MDBCol>
+    </MDBRow>
+
+  </MDBContainer>
   );
 }
