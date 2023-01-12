aa3be7709beeb31d9148f74e49aaa776dfbaa282
DanielMalede <danielmalede@gmail.com>
2023-01-12 17:29:28 +0200

    delete navbar category

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ export default function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <CategoryNavbar/>
         <Routing />
         <Footer />
       </BrowserRouter>
