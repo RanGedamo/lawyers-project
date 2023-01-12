4670db815d0135233f6976e774ab52060243a5d9
NetanelTesfay <netaneltes66@gmail.com>
2023-01-12 19:22:02 +0200

    homepage

--- client/src/component/cards/Category.jsx
+++ client/src/component/cards/Category.jsx
@@ -7,19 +7,19 @@ export default function Category({ categories }) {
       {categories ? (
         categories.map((category) => {
           return (
-            <MDBCol size={4} key={category._id}>
+            <MDBCol size={4} key={category._id} className="d-grid mt-4 md-4">
               <MDBRipple
                 className="bg-image hover-overlay shadow-1-strong rounded"
                 rippleTag="div"
                 rippleColor="light"
               >
-                <img src={category.categoryImg} className="h-25" />
+                <img src={category.categoryImg} className="h-50 d-flex justify-content-evenly" />
                 <Link to={`/category/${category._id}`}>
                   <div
                     className="mask "
                     style={{ backgroundImage: `${category.categoryImg}` }}
                   >
-                    <MDBRow className="text-center flex-column mt-5 pt-5">
+                    <MDBRow className="text-center flex-column mt-4 pt-4">
                       <MDBCol>
                         <MDBTypography className="mb-0 mt-5 pt-5 fw-bold">
                           {category.categoryName}
--- client/src/component/carousel/Crousel.jsx
+++ client/src/component/carousel/Crousel.jsx
@@ -1,29 +1,32 @@
 import { Categories } from "../../sidder";
-import "./carousel.css"
-
-
+import { Lawyers } from "../../sidder";
+import { HomeLawyer } from "../../AppRoute/featuresRoute/home";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+  MDBRipple,
+} from "mdb-react-ui-kit";
+import "./carousel.css";
 
 function Carousel() {
-    return (
-        <div class="slider mb-5 mt-5">
-        <div class="slide-track2">
-          <div class="slide bg-red-500" style={{background:"black"}}>1</div>
-          <div class="slide bg-purple-500">2</div>
-          <div class="slide bg-blue-500">3</div>
-          <div class="slide bg-green-500">4</div>
-          <div class="slide bg-pink-500">5</div>
-          <div class="slide bg-red-500">1</div>
-          <div class="slide bg-purple-500">2</div>
-          <div class="slide bg-blue-500">3</div>
-          <div class="slide bg-green-500">4</div>
-          <div class="slide bg-pink-500">5</div>
-          <div class="slide bg-red-500">1</div>
-          <div class="slide bg-purple-500">2</div>
-          <div class="slide bg-blue-500">3</div>
-          <div class="slide bg-green-500">4</div>
-          <div class="slide bg-pink-500">5</div>
-        </div>
+  return (
+    <div 
+      class="slider mb-5 mt-5 d-flex justify-content-evenly"
+      style={{ height: 100 }}
+    >
+      <div class="slide-track2">
+        <MDBCard className="mw-100">
+          <MDBRipple className="text-center flex-column mt-5 pt-5" >
+            <HomeLawyer lawyers={Lawyers} />
+          </MDBRipple>
+        </MDBCard>
       </div>
-    )}
+    </div>
+  );
+}
 
 export default Carousel;
