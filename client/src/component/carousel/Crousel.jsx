ef0e946e89ef546d69c2fd039a751b27bf75743c
NetanelTesfay <netaneltes66@gmail.com>
2023-01-12 15:04:09 +0200

    carousel

---
+++ client/src/component/carousel/Crousel.jsx
@@ -0,0 +1,29 @@
+import { Categories } from "../../sidder";
+import "./carousel.css"
+
+
+
+function Carousel() {
+    return (
+        <div class="slider">
+        <div class="slide-track2">
+          <div class="slide bg-red-500" style={{background:"black"}}>1</div>
+          <div class="slide bg-purple-500">2</div>
+          <div class="slide bg-blue-500">3</div>
+          <div class="slide bg-green-500">4</div>
+          <div class="slide bg-pink-500">5</div>
+          <div class="slide bg-red-500">1</div>
+          <div class="slide bg-purple-500">2</div>
+          <div class="slide bg-blue-500">3</div>
+          <div class="slide bg-green-500">4</div>
+          <div class="slide bg-pink-500">5</div>
+          <div class="slide bg-red-500">1</div>
+          <div class="slide bg-purple-500">2</div>
+          <div class="slide bg-blue-500">3</div>
+          <div class="slide bg-green-500">4</div>
+          <div class="slide bg-pink-500">5</div>
+        </div>
+      </div>
+    )}
+
+export default Carousel;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,58 +1,74 @@
 // import Category from '../../component/cards/Category';
 import {
-   MDBContainer, 
-   MDBRow, 
-   MDBCol,  
-   MDBCard,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
   MDBCardBody,
   MDBCardText,
-  MDBCardImage
-   } from 'mdb-react-ui-kit';
-import { Hero, Scheduling, HowToHire, SupportBoard, Category, ContactUs, HomeLawyer} from "../../AppRoute/featuresRoute/home.js"
-import {Categories} from "../../sidder"
+  MDBCardImage,
+} from "mdb-react-ui-kit";
+import {
+  Hero,
+  Scheduling,
+  HowToHire,
+  SupportBoard,
+  Category,
+  ContactUs,
+  HomeLawyer,
+} from "../../AppRoute/featuresRoute/home.js";
+import { Categories } from "../../sidder";
+import Carousel from "../../component/carousel/Crousel.jsx";
 
 export default function Home() {
-
   return (
-    <MDBContainer>
+    <MDBContainer className="w-100">
       <br />
-      <MDBContainer className="mb-4" >
-      <Hero/>
+      <MDBContainer className="mb-8">
+        <Hero />
       </MDBContainer>
       <br />
-    <MDBRow className='mb-4 rounded-7'>
-    <SupportBoard/>
-    </MDBRow>
-    <br />
-    <MDBCol className='d-grid'>
-    {
-      Categories.map((key) =>{
-      return(
-    <MDBCard className="d-flex align-items-start bg-light mb-3">
-      <MDBCardImage src={key.categoryImg} position='top' />
-      <MDBCardBody id={key.id}>
-        <MDBCardText>
-          {key.categoryName}
-        </MDBCardText>
-      </MDBCardBody>
-    </MDBCard>
-      )
-})}
-  </MDBCol>
-    <br />
-    <MDBRow className='mb-3d d-flex justify-content-around rounded-fluid'>
-        <MDBCol size='6' md='4'>
-          <ContactUs/>
-        </MDBCol>
-        <MDBCol size='6' md='4'>
-          <Scheduling/>
-        </MDBCol>
+      <MDBRow className="mb-8 rounded-8">
+        <SupportBoard />
       </MDBRow>
       <br />
-      <MDBRow className='mb-8 d-flex justify-content-around rounded-7-fluid'>
-      <HowToHire />
-    </MDBRow>
-    <br />
-  </MDBContainer>
+      <MDBContainer className="d-flex justify-content-center">
+        <MDBRow>
+          {Categories.map((key) => {
+            return (
+              <MDBCol size={4}>
+                <MDBCard>
+                  <MDBCardImage
+                    className=""
+                    src={key.categoryImg}
+                    alt=""
+                    position="top"
+                  />
+                  <MDBCardBody id={key.id}>
+                    <MDBCardText>{key.categoryName}</MDBCardText>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+            );
+          })}
+        </MDBRow>
+      </MDBContainer>
+      <br />
+      <Carousel/>
+      <MDBRow className="mb-3d d-flex justify-content-around rounded-fluid">
+        <MDBCol size="6" md="4">
+          <ContactUs />
+        </MDBCol>
+        <MDBCol size="6" md="4">
+          <Scheduling />
+        </MDBCol>
+      </MDBRow>
+      <Carousel/>
+      <br />
+      <MDBRow className="mb-8 d-flex justify-content-around rounded-7-fluid">
+        <HowToHire />
+      </MDBRow>
+      <br />
+    </MDBContainer>
   );
 }
