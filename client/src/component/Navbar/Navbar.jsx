c79df771fd23da493fcfca9af0ec9ad26fb1d8c6
Batel Haiylo <inpo686@gmail.com>
2023-01-13 04:32:27 +0200

    category page-not done yet + navbar remove category

--- client/src/component/Navbar/Navbar.jsx
+++ client/src/component/Navbar/Navbar.jsx
@@ -1,59 +1,65 @@
-import { Box,Flex, Avatar,Button,Menu,MenuButton,MenuList,MenuItem,MenuDivider,useDisclosure,useColorModeValue,Stack,useColorMode,Center,} from '@chakra-ui/react';
-import { ColorModeSwitcher } from '../../ColorModeSwitcher';
+import {
+  Box,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "../../ColorModeSwitcher";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PopUpRole from "../pop/PopUpBtn";
 import Chat from "../chat/Chat";
 
-
 export default function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [bool,setBool] = useState(false)
+  const [bool, setBool] = useState(false);
 
   const SignIn = () => {
-    setBool(true)
-  }
+    setBool(true);
+  };
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Link
-              to={'/'}
-          >
-          <Box>Themis</Box>
-          </Link>
-          <Link
-              to={'/category/:id'}
-          >
-          <Box>Categories</Box>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Link to={"/"}>
+            <Box>Themis</Box>
           </Link>
 
-          <Flex alignItems={'center'}>
-            <Stack direction={'row'} spacing={7}>
-
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
               <Menu>
                 <Stack
                   flex={{ base: 1, md: 0 }}
-                  justify={'flex-end'}
-                  direction={'row'}
+                  justify={"flex-end"}
+                  direction={"row"}
                   spacing={6}
                 >
-              <ColorModeSwitcher justifySelf="flex-end" />
+                  <ColorModeSwitcher justifySelf="flex-end" />
                   <Button
-                    as={'a'}
-                    fontSize={'sm'}
+                    as={"a"}
+                    fontSize={"sm"}
                     fontWeight={400}
-                    color={'white'}
-                    bg={'blue.400'}
-                    variant={'link'}
-                    href={'#'}
+                    color={"white"}
+                    bg={"blue.400"}
+                    variant={"link"}
+                    href={"#"}
                     _hover={{
-                      bg: 'pink.300',
+                      bg: "pink.300",
                     }}
                     onClick={SignIn}
                   >
                     Sign In
-                    {bool?<PopUpRole/>:""}
+                    {bool ? <PopUpRole /> : ""}
                   </Button>
                 </Stack>
               </Menu>
@@ -61,7 +67,10 @@ export default function Navbar() {
           </Flex>
         </Flex>
       </Box>
-      <div className="chat d-flex sticky-top" style={{ height: "0px", justifyContent: "end", width: "100" }}>
+      <div
+        className="chat d-flex sticky-top"
+        style={{ height: "0px", justifyContent: "end", width: "100" }}
+      >
         <Chat />
       </div>
     </>
---
+++ client/src/component/Section/SubCategory.jsx
@@ -0,0 +1,128 @@
+import React from "react";
+import {
+  Box,
+  Heading,
+  Link,
+  Image,
+  Text,
+  useColorModeValue,
+  Flex,
+} from "@chakra-ui/react";
+import Blob from "../Shape/Blob";
+
+// <SimpleGrid
+//   columns={{ base: 1, md: 2 }}
+//   spacing={10}
+//   key={subCategory._id}
+// >
+//   <Box>
+//     <Text fontSize={"xl"} color={"gray.400"}>
+//       <Text as={"span"} fontWeight={700} color={"white"}>
+//         {subCategory.name}
+//       </Text>
+//     </Text>
+//   </Box>
+// </SimpleGrid>
+
+export default function SubCategory({ subCategory }) {
+  return (
+    <Box
+      marginTop={{ base: "1", sm: "5" }}
+      display="flex"
+      flexDirection={{ base: "column", sm: "row" }}
+      justifyContent="space-between"
+      key={subCategory._id}
+    >
+      <Box
+        display="flex"
+        flex="1"
+        marginRight="3"
+        position="relative"
+        alignItems="center"
+      >
+        <Box
+          width={{ base: "100%", sm: "85%" }}
+          zIndex="2"
+          marginLeft={{ base: "0", sm: "5%" }}
+          marginTop="5%"
+        >
+          <Flex
+            flex={1}
+            justify={"center"}
+            align={"center"}
+            position={"relative"}
+            w={"full"}
+          >
+            <Blob
+              w={"550%"}
+              h={"550%"}
+              position={"absolute"}
+              top={"-220%"}
+              left={0}
+              zIndex={-1}
+              color={useColorModeValue("blue.50", "blue.400")}
+            />
+            <Box
+              position={"relative"}
+              height={"100"}
+              width={"full"}
+              overflow={"hidden"}
+            >
+              <Image
+                alt={"sub category Image"}
+                fit={"cover"}
+                align={"center"}
+                w={"100%"}
+                h={"100%"}
+                src={subCategory.img}
+              />
+            </Box>
+          </Flex>
+
+          {/* <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+              <Image
+                borderRadius="lg"
+                src={subCategory.img}
+                alt="sub category"
+                objectFit="contain"
+              />
+            </Link> */}
+        </Box>
+        <Box zIndex="1" width="100%" position="absolute" height="100%">
+          <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+            <Box
+              bgGradient={useColorModeValue(
+                "radial(orange.600 1px, transparent 1px)",
+                "radial(orange.300 1px, transparent 1px)"
+              )}
+              backgroundSize="20px 20px"
+              opacity="0.4"
+              height="100%"
+            />
+          </Link>
+        </Box>
+      </Box>
+      <Box
+        display="flex"
+        flex="1"
+        flexDirection="column"
+        justifyContent="center"
+        marginTop={{ base: "3", sm: "0" }}
+      >
+        <Heading marginTop="1">
+          <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+            {subCategory.name}
+          </Link>
+        </Heading>
+        <Text
+          as="p"
+          marginTop="2"
+          color={useColorModeValue("gray.700", "gray.200")}
+          fontSize="lg"
+        >
+          {subCategory.services}
+        </Text>
+      </Box>
+    </Box>
+  );
+}
