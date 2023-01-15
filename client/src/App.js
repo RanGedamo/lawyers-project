6136f22c11bb0a462284e210e3ce93d24173c3d5
DanielMalede <danielmalede@gmail.com>
2023-01-15 23:46:42 +0200

    sign in

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
+        {/* <Navbar /> */}
         <CategoryNavbar/>
         <Routing />
         <Footer />
--- client/src/component/Navbar/CategoryNavbar.jsx
+++ client/src/component/Navbar/CategoryNavbar.jsx
@@ -16,6 +16,8 @@ import {
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 import { Categories } from "../../sidder";
+import PopUpRole from "../pop/PopUpBtn";
+import { ColorModeSwitcher } from "../../ColorModeSwitcher";
 
 export default function CategoryNavbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -30,18 +32,40 @@ export default function CategoryNavbar() {
           display={{ md: "none" }}
           onClick={isOpen ? onClose : onOpen}
         />
+        <Link to={"/"}>
+          <>Themisss</>
+        </Link>
         <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
           {Categories.map((category, i) => (
-            <Link key={i} to={`/category/${category._id}`}>{category.categoryName}</Link>
-          ))}{" "}
+            <Link key={i} to={`/category/${category._id}`}>
+              {category.categoryName}
+            </Link>
+          ))}
         </HStack>
-      </Flex>
 
+        <Flex alignItems={"center"}>
+          <Stack direction={"row"} spacing={7}>
+            <Menu>
+              <Stack
+                flex={{ base: 1, md: 0 }}
+                justify={"flex-end"}
+                direction={"row"}
+                spacing={6}
+              >
+                <ColorModeSwitcher justifySelf="flex-end" />
+                <PopUpRole />
+              </Stack>
+            </Menu>
+          </Stack>
+        </Flex>
+      </Flex>
       {isOpen ? (
         <Box pb={4} display={{ md: "none" }}>
           <Stack as={"nav"} spacing={4}>
             {Categories.map((category, i) => (
-              <Link key={i} to={`/category/${category._id}`}>{category.categoryName}</Link>
+              <Link key={i} to={`/category/${category._id}`}>
+                {category.categoryName}
+              </Link>
             ))}{" "}
           </Stack>
         </Box>
