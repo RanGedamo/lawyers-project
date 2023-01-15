6098b6c3cc801528941b8efc13d1b9972bbf53bf
Batel Haiylo <inpo686@gmail.com>
2023-01-15 15:20:15 +0200

    upload image

--- client/src/component/Searchbar.jsx
+++ client/src/component/Searchbar.jsx
@@ -1,34 +1,9 @@
 import { LawyerFake } from "../sidder";
-import React from "react";
-import { useState } from "react";
-import HomeLawyer from "../component/cards/HomeLawyer"
+import react,{ useState } from "react";
+import { MDBInput } from 'mdb-react-ui-kit';
 
-export default function Searchbar() {
-  const [lawyers, setLawyers] = useState(LawyerFake);
-
-//   rate: 5,
-//   location: "Rehovot",
-//   prize: 500,
-//   experience: 5,
-//   available: true,
-//   category: "Family",
-//   avgTime: 5,
-//   workDueTime: 4,
-
-  const Filter = (arr,rate,location,prize,experience,available,category,avgTime,workDueTime) => {
-return arr.filter(lawyers => 
-    rate === lawyers.rate &&
-    location === lawyers.location &&
-    prize === lawyers.prize &&
-    experience === lawyers.experience &&
-    available === true &&
-    category === lawyers.category &&
-    avgTime === lawyers.avgTime &&
-    workDueTime === lawyers.workDueTime
-    ) 
-
-  }
+export default function App() {
   return (
-
-  )
-};
+    <MDBInput label='Example label' id='form1' type='text' />
+  );
+}
