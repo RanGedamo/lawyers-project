ca313911fff27e1492a3789d139a2e1d08939ccc
RanGedamo <mcrangedamo@gmail.com>
2023-01-17 01:36:04 +0200

    fix service

--- client/src/services/emailHandler.js
+++ client/src/services/emailHandler.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = "http://localhost:6060/emailHandler";
 
 export const postContactUs = async (contactUser) => 
 {
-  const response = await axios.post("/contactUs",contactUser)
+  const response = await axios.post("http://localhost:6060/emailHandler/contactUs",contactUser)
 
   const category = await response.data;
   return category;
--- client/src/services/reviewsService.js
+++ client/src/services/reviewsService.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = "http://localhost:6060/review";
 
 export const createReview=async(data,email)=>{
 
-    const response=await axios.post(`/${email}`,{
+    const response=await axios.post(`http://localhost:6060/review/${email}`,{
         userEmail:data.userEmail,
         comments:data.comments,
         image:data.image,
@@ -18,7 +18,7 @@ export const createReview=async(data,email)=>{
 }
 
 export const deleteReview=async(id)=>{
-const response=await axios.delete(`/${id}`)
+const response=await axios.delete(`http://localhost:6060/review/${id}`)
 const deleteDate=await response.data
 return deleteDate;
 }
