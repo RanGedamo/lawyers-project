013af4bf4b23a810347ecbb0c95457e548c2839a
NetanelTesfay <netaneltes66@gmail.com>
2023-01-17 04:53:07 +0200

    category changes

--- client/src/component/cards/Category.jsx
+++ client/src/component/cards/Category.jsx
@@ -1,17 +1,18 @@
+import { Skeleton, Stack } from "@chakra-ui/react";
 import { MDBCol, MDBRipple, MDBRow, MDBTypography } from "mdb-react-ui-kit";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getCategory } from "../../services/categoryService";
-import './Category.css'
-export default function Category({categories}) {
+import "./Category.css";
+
+export default function Category({ categories }) {
   // const [category, setCategory] = useState();
   // useEffect(() => {
   //   getCategory().then((res) => setCategory(res));
   // }, []);
   return (
-    <MDBRow className=" row-cols-md-2 row-cols-lg-3 ">
+    <>
       {categories ? (
-        
         categories?.map((categories, i) => {
           return (
             <MDBCol size={12} key={i} className="mt-4 ">
@@ -24,23 +25,31 @@ export default function Category({categories}) {
                   src={categories.categoryImg}
                   className="w-100 imagecsscategory"
                 />
-                <Link to={`/category/${categories._id}`}>                  
+                <Link to={`/category/${categories._id}`}>
                   <div
                     className="mask "
-                    style={{ backgroundColor: "rgba(251, 251, 251, 0.8)" }}
+                    style={{ backgroundColor: "rgba(251, 251, 251, 0.9)" }}
                   >
-                    <p className="lead mb-0 text-dark fw-bolder" >{categories.name}</p>
-                    <br />
-                    <p className="lead mb-0 d-flex text-dark fs-bolder" >{categories.description}</p>
+                    {categories.subCategory.map((category, j) => {
+                      return (
+                        <ul className="text-center d-flex justify-content-evenly mt-4">
+                          <li>{category.name}</li>
+                        </ul>
+                      );
+                    })}
                   </div>
-                  </Link>
+                </Link>
               </MDBRipple>
             </MDBCol>
           );
         })
       ) : (
-        <MDBCol>no data found</MDBCol>
       )}
-    </MDBRow>
+        <Stack>
+          <Skeleton height="20px" />
+          <Skeleton height="20px" />
+          <Skeleton height="20px" />
+        </Stack>
+    </>
   );
 }
