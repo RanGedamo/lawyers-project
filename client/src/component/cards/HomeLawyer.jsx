6ec6974c8d2a29af0a4f64849aab3ea66843e79a
NetanelTesfay <netaneltes66@gmail.com>
2023-01-18 02:55:28 +0200

    subcategory

--- client/src/component/cards/HomeLawyer.jsx
+++ client/src/component/cards/HomeLawyer.jsx
@@ -19,13 +19,13 @@ export default function HomeLawyer({ lawyers, id }) {
     <MDBRow onClick={sendToCoockies}>
       {lawyers ? (
         lawyers.map((lawyer, i) => (
-          <MDBCol key={i} size={3} className="mb-5 col-md-6 col-lg-5 mt-4 ">
+          <MDBCol key={i} style={{height:""}} className="mb-4 col-lg-6 m-6 col-sm-12">
             <MDBRipple
-              className="bg-image hover-overlay shadow-1-strong rounded-8"
+              className="bg-image hover-overlay shadow-1-strong rounded-5"
               rippleTag="div"
               rippleColor="light"
             >
-              <img src={lawyer.imageString} alt="lawyer" className="h-10" />
+              <img src={lawyer.imageString} alt="lawyer" className="w-100 imagecsscategory"/>
               <Link to={`/lawyer/profile/${lawyer.email}`}>
                 <div
                   className="mask "
@@ -33,15 +33,16 @@ export default function HomeLawyer({ lawyers, id }) {
                 >
                   <MDBRow className="text-center flex-column mt-5 pt-5">
                     <MDBCol>
-                      <MDBTypography className="mb-0 mt-5 pt-5 fw-bold" style={{ color: "rgb(46, 133, 232)" }}>
+                      <MDBTypography className="mb-1 mt-5 pt-5 fw-bold" style={{ color: "rgb(46, 133, 232)" }}>
                         My name is {lawyer.firstName} {lawyer.lastName}
                       </MDBTypography>
                     </MDBCol>
                     <MDBCol>
-                      <MDBTypography className="text-white mt-2">
+                      <MDBTypography className="text-white mt-2 mb-4">
                         hourly fee: {lawyer.price}$
                         <br />
                         years of experience: {lawyer.experience}
+                        <br />
                         {lawyer.available ? (
                           <MDBBadge pill light color="success">
                             {" "}
